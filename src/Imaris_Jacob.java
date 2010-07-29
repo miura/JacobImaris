9a9ebe3cd8cd075e10714f10839f48d858727768
kotamiura <kotamiura@0ce82a30-2411-ce4f-b84c-be783ca2c29f>
2010-07-29 08:52:17 +0000

    "hello Imaris" by Volker untouched yet

---
+++ src/Imaris_Jacob.java
@@ -0,0 +1,12 @@
+import helloImaris.ImarisRemoteControl;
+import ij.plugin.PlugIn;
+
+
+public class Imaris_Jacob implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		ImarisRemoteControl view = new ImarisRemoteControl();
+		view.show();
+	}
+}
