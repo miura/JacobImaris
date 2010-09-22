943dfdb1a86fd776392c8729c13de7d9ac755922
kotamiura <kotamiura@0ce82a30-2411-ce4f-b84c-be783ca2c29f>
2010-09-22 11:17:35 +0000

    commit belated

--- src/emblcmci/ImarisRemoteExport.java
+++ src/emblcmci/ImarisRemoteExport.java
@@ -49,10 +49,28 @@ import java.awt.Dimension;
 import java.awt.Rectangle;
 /**
  * @author Kota Miura
- * Modified "ImarisRemoteControl.java (by Volker Baeker, published in Bitplane website)" 
- * To export of stack in IJ could be exported to Imaris.
+ * Based on "ImarisRemoteControl.java" by Volker Baeker
+ * 	see http://www.bitplane.com/ImarisXT/HelloImarisDataSetJACOB.zip
+ * 
+ * This plugin exports stacks showed in IJ directly to Imaris.
  *   
  * TODO 20100804 16bit does not work, so debug. 
+ * 
+ * Copyright © 2010 Kota Miura
+ * License: GPL 2
+ *    
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
  */
 
 @SuppressWarnings("serial")
